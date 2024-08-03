fbef923d4cb44456b81005df441727fa753ae863
Sanket Desai <user15@sunrichtech.com>
2024-08-03 19:37:24 +0530

    Initial commit

---
+++ src/main/java/org/stpl/learningspringsecurity/MyCustomerFilter.java
@@ -0,0 +1,4 @@
+package org.stpl.learningspringsecurity;
+
+public class MyCustomerFilter {
+}
---
+++ src/test/java/org/stpl/learningspringsecurity/LearningSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package org.stpl.learningspringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
